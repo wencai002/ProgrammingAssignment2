8a1b8b078d973f402ada36abc0823e399965090d
wencai002 <45918283+wencai002@users.noreply.github.com>
2018-12-26 13:48:22 +0100

    Change Test 20181226
    First test for the commit change

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_x <- NULL
+    if (nrow(x)!=ncol(x)){
+        simpleError(print("matrix given has to be square"))
+    }
+    else {
+        set <- function(y) {
+            x <<- y
+            inv_x <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) inv_x <<- inv
+        getinv <- function() inv_x
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+    }
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv_x <- x$getinv()
+    if(!is.null(inv_x)) {
+        message("getting cached data")
+        return(inv_x)
+    }
+    data <- x$get()
+    inv_x <- solve(data, ...)
+    x$setinv(inv_x)
+    inv_x
 }
